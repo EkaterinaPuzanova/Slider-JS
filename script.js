3ec9c2b9d8a29556da04b1ecce6e7a6935961ff1
katya_puzan <katesamek357@gmail.com>
2023-03-05 23:08:33 +0300

    refactor: change structure of the project

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-import { data } from '/js/data.js';
+let data = [
+    {
+        subtitleCity: `Rostov-on-Don 
+        LCD admiral`,
+        subtitleApart: "81 m2",
+        subtitleTime: "3.5 months"
+    }, {
+        subtitleCity: `Sochi 
+        Thieves`,
+        subtitleApart: "105 m2",
+        subtitleTime: "4 months"
+    }, {
+        subtitleCity: `Rostov-on-Don 
+        Patriotic`,
+        subtitleApart: "93 m2",
+        subtitleTime: "3 months"
+    }
+];
 
 function initSlider() {
 
